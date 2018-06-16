4b11d1e0270cb812bd00b44975ac7a412f105db3
Jennifer Pazos <jmp521@gmail.com>
2018-06-16 01:19:01 -0500

    created the div that will hold each todo, including buttons

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
-
-
+let count = 0
+let todo_array = []
 addTodo = (event) =>  {
   event.preventDefault()
-  const test = document.getElementById("todo").value
-  console.log(test)
-  return false;
+  const value = document.getElementById("todo").value
+
+  document.getElementById("todo").value = ""
+}
+
+
+
+addTodoToArray = (todo) => {
+    addTodoToArray
+}
+
+
+updateList = (value) => {
+  text = document.createElement("P")
+  node = document.createTextNode(value)
+  text.appendChild(node)
+  document.getElementById("todo_list").appendChild(text)
 }
