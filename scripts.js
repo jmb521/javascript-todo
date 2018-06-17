b70d969c7bd4313b09592870eadda7094b28a9ee
Jennifer Pazos <jmp521@gmail.com>
2018-06-16 19:20:14 -0500

    added the jquery append function to take the styled div we want to hold and append it to the container div

--- scripts.js
+++ scripts.js
@@ -1,22 +1,52 @@
 let count = 0
-let todo_array = []
+
 addTodo = (event) =>  {
   event.preventDefault()
   const value = document.getElementById("todo").value
-
+  addTodoToArray(value)
   document.getElementById("todo").value = ""
 }
 
 
 
 addTodoToArray = (todo) => {
-    addTodoToArray
+  count = count += 1
+  const new_todo = {
+    id: count,
+    text: todo,
+  }
+
+    updateList(new_todo)
+    return count
 }
 
 
 updateList = (value) => {
-  text = document.createElement("P")
-  node = document.createTextNode(value)
-  text.appendChild(node)
-  document.getElementById("todo_list").appendChild(text)
+      $(".todos").append(
+      `<div class="todo_list ${value.id}">
+        <div class="todo_value">
+          <h3>${value.text}</h3>
+        </div>
+        <div class="buttons">
+          <button class="completed" onclick="completed(${value.id})" name="Completed" value="Completed">Completed</button>
+          <button class="delete" onclick="deleteTodo(${value.id})" name="delete" value="Delete">Delete</button>
+        </div>
+      </div>`
+
+    )
+  }
+
+completed = (id) => {
+
+  const found_todo = document.getElementsByClassName(id)
+  found_todo.elm.style(text-decoration) = "line-through"
+}
+
+deleteTodo = (id) => {
+  const int_id = parseInt(id, 10)
+  $("." + int_id).remove()
+  // $(""." + int_id").remove()
+
+
+
 }
